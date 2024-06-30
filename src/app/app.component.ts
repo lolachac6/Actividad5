b4e4e3f25c75a45220885040bf8ee38322e54b21
Lola <lola_chacon8@hotmail.com>
2024-06-30 20:43:35 +0200

    componentes importados
    works!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormularioComponent,ListadoNoticiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
