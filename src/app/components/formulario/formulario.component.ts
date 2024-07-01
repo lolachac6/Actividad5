d14210c04fafcbd4a438e77eb14d931f3af26def
Lola <lola_chacon8@hotmail.com>
2024-07-01 12:12:07 +0200

    @Output() realizado en formulario
    + funcion

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Inoticias } from '../../interfaces/inoticias.interface';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  newNoticia: Inoticias = {titulo:"",imagen:"", texto:"",fecha:""}
+ // @Output() noticiaEmitir: EventEmitter<Inoticias> = new EventEmitter()
+  guardarNoticia():void{
+    console.log(this.newNoticia)
+  }
 
 }
